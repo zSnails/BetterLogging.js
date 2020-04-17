50ea78a8187edf6eba6503df03f222f4e6c4aa73
Snails <zSnails>
2020-04-16 23:53:48 -0600

    new way of setting the level

--- src/tests/test_logs.js
+++ src/tests/test_logs.js
@@ -3,7 +3,7 @@ const options = {
     name: 'tests',
     //filename: 'output',
     //filemode: 'a',
-    level: 'DEBUG',
+    level: process.argv[2] || 'debug',
     format: '#{time} |#{name}| #{levelname} > #{message}',
 }
 
@@ -16,4 +16,4 @@ logger.debug('Testing', 'some', 'stuff');
 logger.warning('warning');
 logger.error('err');
 logger.critical('can\'t work as intended');
-console.log(logger.getLevelInteger)
+console.log(logger.levelInteger)
